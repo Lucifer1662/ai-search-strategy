6484553c7732120d9512282f7c202cbd265b651f
Will <lordwilliam94@gmail.com>
2020-03-12 15:00:55 +1100

    Good Luck possible_moves

--- PathFinding.py
+++ PathFinding.py
@@ -2,18 +2,46 @@
 def isInsideBoard(pos):
     return pos[0] < 8 and pos[0] >= 0 and pos[1] < 8  and pos[1] >= 0
 
-def CanMakeMove(pos, move, blackBoard):
-    newPos = pos+move
-    return not newPos in blackBoard and isInsideBoard(newPos) 
+def CanMakeMove(move, colour, board):
+    if not isInsideBoard(move):
+        return False
+    try:
+        return board[move][0] == colour
+    except KeyError:
+        return True
 
-def AvailableActions(numTokens, pos, blackBoard):
+def upper_bound_moves(numTokens, pos, blackBoard=None):
     moves = []
     for step in range(1,numTokens+1):
         for dir in [(step,0),(-step,0),(0,step),(0,-step)]:
-            if(CanMakeMove(pos, dir, blackBoard)):
-                for amountOfTokens in range(1,numTokens+1):
-                    moves.append((amountOfTokens, pos, pos+dir))
+            #if(CanMakeMove(pos, dir, blackBoard)):
+            move = (pos[0] + dir[0], pos[1] + dir[1])
+            if not isInsideBoard(move):
+                continue
+            for amountOfTokens in range(1,numTokens+1):
+                moves.append((amountOfTokens, move))
     return moves
+
+def vector_addition(tup_1: tuple, tup_2: tuple) -> tuple:
+    assert(len(tup_1) == len(tup_2))
+    tup = []
+    for i in range(len(tup_1)):
+        tup.append(tup_1[i] + tup_2[i])
+    return tuple(tup)
+
+def possible_moves(numTokens: int, pos: tuple, colour: str, board: dict) -> dict:
+    moves = []
+    for step in range(1,numTokens+1):
+        # List of all possible moves
+        for dir in [(step,0),(-step,0),(0,step),(0,-step)]:
+            move = (pos[0]+dir[0],pos[1]+dir[1])
+            print(f"{pos} to {move} can be done: {CanMakeMove(move, colour, board)}")
+            if(CanMakeMove(move, colour, board)):
+                for amountOfTokens in range(1,numTokens+1):
+                    moves.append((amountOfTokens, pos, move))
+    return moves
+
+
 
 
 
